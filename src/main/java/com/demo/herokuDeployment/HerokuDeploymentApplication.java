d69838c382f9e5f08c5f4bc17baf9c0c0a9e49a2
Codesmaster-Lab <codesmaster404@gmail.com>
2021-10-18 13:29:17 +0530

    Initial commmit

---
+++ src/main/java/com/demo/herokuDeployment/HerokuDeploymentApplication.java
@@ -0,0 +1,13 @@
+package com.demo.herokuDeployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuDeploymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuDeploymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/herokuDeployment/HerokuDeploymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.herokuDeployment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDeploymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
